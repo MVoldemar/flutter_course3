655ae9c3213fddb14b6821019a0d81ba00557cb9
MVoldemar <65668419+MVoldemar@users.noreply.github.com>
2021-07-07 14:39:39 +0300

    3 generation

--- lib/pages/superhero_page.dart
+++ lib/pages/superhero_page.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:superheroes/widgets/action_button.dart';
 
 class SuperheroPage extends StatelessWidget {
   const SuperheroPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-
-        )
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [Center(
+            child: Text(
+              "Hello",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
+          ),
+            ActionButton(text: "", onTap: onTap)
+            ]
+        ),
+      ),
     );
   }
 }
