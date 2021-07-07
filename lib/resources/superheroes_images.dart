5c9e4b7002fdab6eeb65ce3fda13810721cc18d5
MVoldemar <65668419+MVoldemar@users.noreply.github.com>
2021-07-07 14:00:02 +0300

    3 generation

--- lib/resources/superheroes_images.dart
+++ lib/resources/superheroes_images.dart
@@ -4,4 +4,6 @@ class SuperheroesImages {
   const SuperheroesImages._();
 
   static const ironman = "assets/images/ironman.png";
+  static const hulk = "assets/images/hulk.png";
+  static const supernman = "assets/images/superman.png";
 }
